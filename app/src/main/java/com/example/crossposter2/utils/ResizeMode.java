17b8b3bdf43691e21cd8dbec3028f10d0d2a6204
WelterAppleseed <threedaystomars@yandex.ru>
2021-08-30 20:35:42 +0300

    Clean and build release version of project.
    Signed-off-by: WelterAppleseed <threedaystomars@yandex.ru>

---
+++ app/src/main/java/com/example/crossposter2/utils/ResizeMode.java
@@ -0,0 +1,6 @@
+package com.example.crossposter2.utils;
+
+enum ResizeMode {
+    EQUAL_OR_GREATER,
+    EQUAL_OR_LOWER
+}
---
+++ app/src/main/java/com/example/crossposter2/utils/ScaleMode.java
@@ -0,0 +1,6 @@
+package com.example.crossposter2.utils;
+
+enum ScaleMode {
+    EQUAL_OR_GREATER,
+    EQUAL_OR_LOWER
+}
